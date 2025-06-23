c3686f38350058b2bf72de4abeecd20405ca40e3
kannoncoding <codingadaptations@gmail.com>
2025-06-22 20:01:59 -0600

    initial setup

---
+++ CelebracionesInternacionales/src/celebracionesInternacionales/Principal.java
@@ -0,0 +1,8 @@
+
+package celebracionesInternacionales;
+
+public class Principal {
+    public static void main(String[] args) {
+        System.out.println("¡Proyecto Celebraciones Internacionales iniciado!");
+    }
+}
